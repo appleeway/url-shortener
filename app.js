402f9af4bae45d85cfb6d54ade2998a0fe9ca5a8
Appleeway <applee62002@gmail.com>
2020-04-17 17:14:38 +0800

    Add route for short Url

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
+
+mongoose.connect('mongodb://localhost/urlShortener', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
 
 // set view engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.render('index')
+
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find().lean()
+  res.render('index', { shortUrls: shortUrls })
+})
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+  res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
 })
 
 //listening port
